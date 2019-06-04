020e7a2006da8f7700ff67c876d3c53c4dfb8db4
Henry Huck <henryhuck@hotmail.fr>
2019-06-04 15:34:25 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 
 class NameSpace {
   
- test = (one, two, tree, four, five, six, seven) => {
+ test = (one, two, tree, four, five, six, seven ) =>  
+ {
   return one + two + three + four + five + six + seven;
  };
 
