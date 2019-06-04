a92e62660f9c29372795a698e411df1721d936cb
Henry Huck <henryhuck@hotmail.fr>
2019-06-04 16:44:38 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,131 +1,115 @@
-
 class NameSpace {
-  
- test = (one, two, tree, four, five, six, seven ) =>  
- {
-  return one + two + three + four + five + six + seven;
- };
+  test = (one, two, three, four, five, six, seven) => {
+    return one + two + three + four + five + six + seven;
+  };
 
- test = (one, two, tree, four, five, six, seven) => {
-  return one + two + three + four + five + six + seven;
- };
-   
- test   = ( one,two, tree, four, five, six, seven) =>  {
-   
-   
-   
-   
-      
-   let a = one;
-   a = one;
-   a = one;
+  test = (one, two, three, four, five, six, seven) => {
+    return one + two + three + four + five + six + seven;
+  };
 
-   
-    
-   
-   
-   
-   
-   
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one;
-   a = one; 
-   
-   a = one;
-   a = one;
-   a = one;
-   a = one;
+  test = (one, two, three, four, five, six, seven) => {
+    let a = one;
+    a = one;
+    a = one;
 
-  return one + two + three + four + five + six + seven;
- };
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
+    a = one;
 
- test = (one, two, tree, four, five, six, seven) => {
-  return one + two + three + four + five + six + seven;
- };
+    a = one;
+    a = one;
+    a = one;
+    a = one;
 
-};
+    return one + two + three + four + five + six + seven;
+  };
 
-const mutatting = (anArray) => {
-   anArray.forEach(item => {
-     item.prop = "test";
-   });
-  return anArray;
+  test = (one, two, three, four, five, six, seven) => {
+    return one + two + three + four + five + six + seven;
+  };
 }
+
+const mutatting = anArray => {
+  anArray.forEach(item => {
+    item.prop = "test";
+  });
+  return anArray;
+};
