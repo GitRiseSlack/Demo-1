ea69d3f22a25b9b62df9b8a0c71ced2f1a4a847b
Henry Huck <henryhuck@hotmail.fr>
2019-06-04 14:44:53 +0200

    duplicates

--- demo.js
+++ demo.js
@@ -5,4 +5,16 @@ export class NameSpace {
   return one + two + three + four + five + six + seven;
  };
 
+ test = (one, two, tree, four, five, six, seven) => {
+  return one + two + three + four + five + six + seven;
+ };
+
+ test = (one, two, tree, four, five, six, seven) => {
+  return one + two + three + four + five + six + seven;
+ };
+
+ test = (one, two, tree, four, five, six, seven) => {
+  return one + two + three + four + five + six + seven;
+ };
+
 };
