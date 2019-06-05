bc40addcbcfa34fdc93bc78d8e8c158713b9d470
GitRiseSlack <51406449+GitRiseSlack@users.noreply.github.com>
2019-06-05 14:42:20 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -12,3 +12,11 @@ class NameSpace2 {
     return one + two + three + four + five + six + seven;
   }
 }
+
+class NameSpace3 {
+  
+  test(one, two, three, four, five, six, seven   )
+  {
+    return one + two + three + four + five + six + seven;
+  }
+}
